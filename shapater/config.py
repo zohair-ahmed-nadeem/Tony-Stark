4633292ed32a3365f29455bed4c2bceffe90b04a
zohair ahmed <145790776+zohair-ahmed-nadeem@users.noreply.github.com>
2025-04-18 14:59:25 -0700

    Update config.py

---
+++ shapater/config.py
@@ -0,0 +1,21 @@
+sec = "DC BOT SECRET"
+test_sec = "DC TEST BOT SECRET"
+ai = "GIMINE AI"
+yt = "Google YT API"
+groq_api_key = "OPEN AI API"
+project_ai = 123456790
+owner_id = 123456790
+
+channel_id_wlc_prvt = 123456790
+channel_id_bye_prvt = 123456790
+support_server_id = 123456790
+report_channel_id = 123456790
+channel_id_bot_join = 123456790
+channel_id_status = 123456790
+footer = "Developed BY ZAN ✨"
+
+inv= "https://discord.com/oauth2/authorize?client_id=1184035274037137478&permissions=8&integration_type=0&scope=bot"
+srv = "https://discord.gg/GXuZVF4573"
+link_img_wlc = "https://images-ext-1.discordapp.net/external/UxZYXr5DOtlhmqWMNho_No2EmTe4kL98hQLpZ9QCTjg/https/cdn.dribbble.com/users/1117770/screenshots/2626626/welcome.gif?width=600&height=450"
+
+logo ="<:custom_emoji:1261340401340518552>"
